4c4a3c40dbe73ba47305ae18fca7dfaa44ce28f5
Avelimanuel <aveliemmanuel@gmail.com>
2026-08-04 09:44:55 +0300

    added google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -14,9 +15,9 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Car Key Replacement Nairobi | Auto Locksmith Kenya | AutoKeysExperts",
+  title: "Car Key Replacement Nairobi | Auto Locksmith Kenya | AutoKeyExperts",
   description:
-    "AutoKeysExperts offers fast and reliable car key replacement, car key programming, and emergency car unlocking services in Nairobi and across Kenya. 24/7 mobile auto locksmith near you.",
+    "AutoKeyExperts offers fast and reliable car key replacement, car key programming, and emergency car unlocking services in Nairobi and across Kenya. 24/7 mobile auto locksmith near you.",
 
   keywords: [
     "car key replacement Nairobi",
@@ -28,13 +29,13 @@ export const metadata = {
     "duplicate car key Nairobi",
   ],
 
-  metadataBase: new URL("https://autokeysexperts.co.ke"),
+  metadataBase: new URL("https://autokeyexperts.co.ke"),
 
   openGraph: {
     title: "Car Key Replacement Nairobi | AutoKeysExperts",
     description:
       "24/7 car key replacement and programming services in Nairobi. Fast response, affordable pricing, and professional auto locksmith services.",
-    url: "https://autokeysexperts.co.ke",
+    url: "https://autokeyexperts.co.ke",
     siteName: "AutoKeysExperts",
     locale: "en_KE",
     type: "website",
@@ -56,10 +57,27 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-18362410131"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-ads-tag" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'AW-18362410131');
+          `}
+        </Script>
+      </head>
+
       <body className="min-h-full flex flex-col">
         <Navbar />
         {children}
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
